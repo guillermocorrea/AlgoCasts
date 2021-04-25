4609a4da0b1a972251932ebbccccb25fd7e41285
Luis Correa <luis.correag@perficient.com>
2021-04-25 11:00:19 -0500

    solved maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+
 // --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
@@ -5,6 +7,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // T: O(n), S: O(n)
+  let mostCommonCharacter = "";
+  let mostCommonCharacterFreq = 0;
+  const frequency = {};
+  for (let character of str) {
+    if (frequency[character] !== undefined) {
+      frequency[character]++;
+    } else {
+      frequency[character] = 1;
+    }
+    // How to handle equal frequencies?
+    if (frequency[character] >= mostCommonCharacterFreq) {
+      mostCommonCharacterFreq = frequency[character];
+      mostCommonCharacter = character;
+    }
+  }
+  return mostCommonCharacter;
+}
+
+assert.strictEqual(maxChar("a"), "a");
+assert.strictEqual(maxChar("abcdefghijklmnaaaaa"), "a");
+assert.strictEqual(maxChar("ab1c1d1e1f1g1"), "1");
 
 module.exports = maxChar;
