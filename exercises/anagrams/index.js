c9fd758231c92c0fcef4ac558ad9c4d9471a582e
Guillermo Correa <guillermo.correa.99@gmail.com>
2021-04-25 19:02:02 -0500

    add anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/**
+ * T: O(n + m), S: O(n + m)
+ * @param {string} stringA
+ * @param {string} stringB
+ * @returns {boolean}
+ */
+function anagrams(stringA, stringB) {
+  const cleanA = cleanStr(stringA);
+  const cleanB = cleanStr(stringB);
+  if (cleanA.length !== cleanB.length) {
+    return false;
+  }
+  const charMapA = buildMap(cleanA);
+  const charMapB = buildMap(cleanB);
+
+  for (let charA in charMapA) {
+    if (charMapA[charA] !== charMapB[charA]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const cleanStr = (str) => str.replace(/[^\w]/g, '').toLowerCase();
+
+function buildMap(str) {
+  const map = {};
+  for (let character of str) {
+    map[character] = map[character] + 1 || 1;
+  }
+  return map;
+}
 
 module.exports = anagrams;
